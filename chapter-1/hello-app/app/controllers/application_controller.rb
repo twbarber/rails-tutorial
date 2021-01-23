a14dad2a2216bab0d93613ee2a849e8a834910f2
Tyler Barber <dev@twbarber.com>
2021-01-23 17:14:29 -0500

    Complete Chapter 1 Exercises

--- chapter-1/hello-app/app/controllers/application_controller.rb
+++ chapter-1/hello-app/app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
 
     def hello
-        render html: "hello, world!"
+        render html: "¡Hola, mundo!"
+    end
+
+    def goodbye
+        render html: "goodbye, world!"
     end
 
 
