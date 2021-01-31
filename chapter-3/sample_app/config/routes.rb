97222b868a5db65845f96768586291ebc1f0132a
Tyler Barber <dev@twbarber.com>
2021-01-30 22:21:19 -0500

    Fix routes

--- chapter-3/sample_app/config/routes.rb
+++ chapter-3/sample_app/config/routes.rb
@@ -6,7 +6,5 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=begin
   root 'static_pages#home'
-=end
 end
